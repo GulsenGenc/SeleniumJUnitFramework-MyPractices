bfa31001176ca7eff4adc89d557597fe2149fa4f
Gulsen Genc <gulsengenctuncer@gmail>
2022-05-15 23:56:24 +0300

    15.05.2022

---
+++ src/test/java/deneme.java
@@ -0,0 +1,17 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class deneme {
+    @Test
+    public void denemeTesti(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.twitter.com");
+    }
+}
