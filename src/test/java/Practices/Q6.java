b0368833438c91ce53041a1cd3d4ba5e4cb01838
Gulsen Genc <gulsengenctuncer@gmail>
2022-05-20 09:18:59 +0300

    20.05.2022

--- src/test/java/Practices/Q6.java
+++ src/test/java/Practices/Q6.java
@@ -1,4 +1,13 @@
 package Practices;
 
 public class Q6 {
+     /*
+   // 1. Amazon.com'a gidelim.
+   // 2. DropDown üzerinde Books secelim.(All yazan yerde)
+   //    kategorilerin hepsini konsola yazdiralim
+   // 3. Arama kutusuna Les Miserables yazalım ve arama yapalim.
+   // 4. Sonuc sayisini ekrana yazdiralim.
+   // 5. Sonucların Les Miserables i icerdigini assert edelim
+   */
+
 }
